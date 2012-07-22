eb97de4cd470e72fd8c0cb9dac48773bfb00a28b
Robby Ranshous <rranshous@gmail.com>
2012-07-22 12:48:34 -0400

    buffering linux dict in memory after first iter run through

--- dictionaries.py
+++ dictionaries.py
@@ -1,16 +1,25 @@
 
+linux_words_buffer = None
 def iter_linux_words():
     """
     iterator of root unique words from linux dict file
     """
-    with open('/usr/share/dict/words','r') as fh:
-        lines = fh.readlines()
-        last = None
-        for word in lines:
-            # just root
-            word = word.strip().split("'")[0]
-            if word != last:
-                yield word
-            last = word
+    global linux_words_buffer
+    if linux_words_buffer:
+        for v in linux_words_buffer:
+            yield v
+    else:
+        internal_buffer = []
+        with open('/usr/share/dict/words','r') as fh:
+            lines = fh.readlines()
+            last = None
+            for word in lines:
+                # just root
+                word = word.strip().split("'")[0]
+                if word != last:
+                    yield word
+                last = word
+                internal_buffer.append(word)
+        linux_words_buffer = internal_buffer
 
 dictionaries = iter_linux_words()
